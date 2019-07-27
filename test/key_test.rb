14c70a28a1773f340d6e857c23a26cc2693ab02f
Scott Payton <spayton.c@gmail.com>
2019-07-26 18:14:03 -0600

    Test complete and working for key string parser

--- test/key_test.rb
+++ test/key_test.rb
@@ -9,4 +9,15 @@ def test_it_exitsts
   assert_instance_of Key, @key
 end
 
+def test_attributes
+  assert_equal "75634", @key.key
+end
+
+def test_key_string_parse
+  assert_equal 75, @key.key_a
+  assert_equal 56, @key.key_b
+  assert_equal 63, @key.key_c
+  assert_equal 34, @key.key_d
+end
+
 end
