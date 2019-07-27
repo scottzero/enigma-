35b84f944c9e7e331afb84aab1795d37601901f4
Scott Payton <spayton.c@gmail.com>
2019-07-27 02:04:58 -0600

    Added requires for offest.rb into test_helper and created offset_test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,5 +4,6 @@ SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/key'
+require './lib/offset'
 require 'pry'
-#require all files here, minitest, simple cov, key, offset,shift, 
+#require all files here, minitest, simple cov, key, offset,shift,
