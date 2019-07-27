f6921e577d1e35e2b8d48c40f636794a80009645
Scott Payton <spayton.c@gmail.com>
2019-07-27 02:35:23 -0600

    Added square_numeric_date method to get a big number, succesfully can offset date

--- lib/offset.rb
+++ lib/offset.rb
@@ -8,4 +8,9 @@ class Offset
     Date.now.strftime('%d%m%y')#this will format the date via the notations in the string
                         #string format method
   end
+
+  #need a method to square the numeric form of our Date
+  def square_numeric_date
+    (@date.to_i ** 2).to_s # ** exponent^...it WORKS! Big NUMBER, reminder ask meg or ian about doubles or longs  :)
+  end
 end
