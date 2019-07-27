4c7e43b33b73be22fbef6d7eed81c32b759957b5
Scott Payton <spayton.c@gmail.com>
2019-07-27 12:26:48 -0600

    Created test for char_set expectation for alphabet array

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -16,6 +16,17 @@ assert_equal @key, @shift.key
 assert_equal @offset, @shift.offset
 end
 
+def test_char_set
+expected = ["a","b","c","d",
+            "e","f","g","h",
+            "i","j","k","l",
+            "m","n","o","p",
+            "q","r","s","t",
+            "u","v","w","x",
+            "y","z"," "]
+assert_equal expected, @shift.char_set
+end
+
 def test_shift_sums_for_key_and_offset
 assert_equal 77, @shift.shift_a
 assert_equal 65, @shift.shift_b
