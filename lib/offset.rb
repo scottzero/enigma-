5165dd85a1a882b9b9aa50009365a2bc6d80fe84
Scott Payton <spayton.c@gmail.com>
2019-07-27 02:22:00 -0600

    Added Method to return current date in correct formatted string

--- lib/offset.rb
+++ lib/offset.rb
@@ -3,4 +3,9 @@ class Offset
   def initialize(date)
     @date = date
   end
-end 
+
+  def current_date
+    Date.now.strftime('%d%m%y')#this will format the date via the notations in the string
+                        #string format method
+  end
+end
