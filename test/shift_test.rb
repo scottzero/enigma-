46e0102ff8a9e6ba66c487e15547b5d8d11017c9
Scott Payton <spayton.c@gmail.com>
2019-07-27 10:37:29 -0600

    Passes test for shift class attributes

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -1,13 +1,19 @@
 require './test/test_helper'
-
 class ShiftTest < Minitest::Test
 def setup
 #need a key, need an offset and new shift object
 @key = Key.new("75634")
 @offset = Offset.new("072719")
-@shift - Shift.new(@key, @offset)
+@shift = Shift.new(@key, @offset)
 end
 
 def test_it_exists
+assert_instance_of Shift, @shift
+end
 
-end 
+def test_attributes
+assert_equal @key, @shift.key
+
+end
+
+end
