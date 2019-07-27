0809a584728327027204b41cd99916fa83ff097e
Scott Payton <spayton.c@gmail.com>
2019-07-26 18:13:27 -0600

    Completed methods for parsing key into 2 digits numbers

--- lib/key.rb
+++ lib/key.rb
@@ -3,4 +3,23 @@ class Key
   def initialize(key = nil)
     @key = key
   end
+
+  #need to have key make its 75634 into 75 a, 56 b, 63 c,34 d
+  def key_a
+    @key[0..1].to_i#takes the string "75634" and returns 75
+  end
+
+  def key_b
+    @key[1..2].to_i
+  end
+
+  def key_c
+    @key[2..3].to_i
+  end
+
+  def key_d
+    @key[3..4].to_i
+  end
+
+
 end
