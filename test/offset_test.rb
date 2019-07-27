f7fd07fad111a3c5d3ad923695d46204e40fb19c
Scott Payton <spayton.c@gmail.com>
2019-07-27 02:11:39 -0600

    Created Test class for offset, passed test for existence

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -1,1 +1,11 @@
+require './test/test_helper'
 
+class OffsetTest < Minitest::Test
+def setup
+@offset = Offset.new("072719")
+end
+
+def test_it_exists
+assert_instance_of Offset, @offset
+end
+end
