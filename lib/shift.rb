e41dfc87b80ee5cf82fec0d710b5c7d5cba0514b
Scott Payton <spayton.c@gmail.com>
2019-07-27 12:06:38 -0600

    Created functions for summing key and offsets

--- lib/shift.rb
+++ lib/shift.rb
@@ -7,6 +7,24 @@ def initialize(key, offset)
   @offset = offset
 end
 
+def shift_a
+@key.key_a + @offset.offset_a
+end
+
+def shift_b
+@key.key_b + @offset.offset_b
+end
+
+def shift_c
+@key.key_c + @offset.offset_c
+end
+
+def shift_d
+@key.key_d + @offset.offset_d
+end
+
+
+
 
 
 end
