b1b7f15a051a073ee8fca84e19ba10d0dca1b6fa
Scott Payton <spayton.c@gmail.com>
2019-07-30 20:01:24 -0600

    working encryption inside of enigma, 100% test coverage

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,4 +3,10 @@ class Enigma
     @date = Offset.new.current_date
     @key = Key.new.key_random
   end
-end 
+
+  def encrypt(message, key = @key, date = @date) #default args to init vars
+    shifts= Shift.new(Key.new(key), Offset.new(date))
+    encryption = shifts.encryption(message)
+    encryption
+  end
+end
---
+++ test/enigma_test.rb
@@ -0,0 +1,16 @@
+require './test/test_helper'
+
+class EnigmaTest < Minitest::Test
+
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
+
+  def test_it_can_encrypt
+    assert_equal "keder ohulw", @enigma.encrypt("hello world", "02715", "040895")
+  end
+end
