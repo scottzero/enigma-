8a477f6444e63e5ee429e3a0f1f324747633ac3d
Scott Payton <spayton.c@gmail.com>
2019-07-31 05:54:24 -0600

    removed comments for code readability, lib files

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,7 +4,7 @@ class Enigma
     @key = Key.new.key_random
   end
 
-  def encrypt(message, key = @key, date = @date) #default args to init vars
+  def encrypt(message, key = @key, date = @date)
     shift_1= Shift.new(Key.new(key), Offset.new(date))
     #adjusted to fit project requirenments
     {
@@ -12,18 +12,14 @@ class Enigma
       :key => shift_1.key.key,
       :date => shift_1.offset.date
     }
-    # encryption = shifts.encryption(message)
-    # encryption
   end
 
-  def decrypt(message, key = @key, date = @date) #default args to init vars
+  def decrypt(message, key = @key, date = @date)
     shift_1= Shift.new(Key.new(key), Offset.new(date))
     {
       :decryption => shift_1.decryption(message),
       :key => shift_1.key.key,
       :date => shift_1.offset.date
     }
-    # decryption = shifts.decryption(message)
-    # decryption
   end
 end
