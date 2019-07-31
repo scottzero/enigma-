84729a11d9d72c25436827aca31a1e949a9f470b
Scott Payton <spayton.c@gmail.com>
2019-07-30 20:53:33 -0600

    passed all tests for decrypt method

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -13,4 +13,8 @@ class EnigmaTest < Minitest::Test
   def test_it_can_encrypt
     assert_equal "keder ohulw", @enigma.encrypt("hello world", "02715", "040895")
   end
+
+  def test_it_can_decrypt
+    assert_equal "hello world", @enigma.decrypt("keder ohulw", "02715", "040895")
+  end
 end
